2472991dc2aecd4b0c90cd915eb32b45fde9581f
DaPiankov <DaPiankov@croc.ru>
2024-02-15 22:22:12 +0300

    [feat]: Добавил базовые зависимости

--- src/main/java/heavenboards/user/service/UserServiceApplication.java
+++ src/main/java/heavenboards/user/service/UserServiceApplication.java
@@ -3,9 +3,17 @@ package heavenboards.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс для запуска сервиса.
+ */
 @SpringBootApplication
 public class UserServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    /**
+     * Главный метод для запуска сервиса.
+     *
+     * @param args - аргументы запуска
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
--- src/test/java/heavenboards/user/service/UserServiceApplicationTests.java
+++ src/test/java/heavenboards/user/service/UserServiceApplicationTests.java
@@ -2,10 +2,13 @@ package heavenboards.user.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
     @Test
     void contextLoads() {
+        System.out.println(123);
     }
 }
