9cfad8d2817afb3b149ae992d15cd31997a6a9f0
DaPiankov <DaPiankov@croc.ru>
2024-02-18 13:02:56 +0300

    [feat]: Добавил тесты на регистрацию и аутентификацию

--- src/test/resources/sql/user/create.sql
+++ src/test/resources/sql/user/create.sql
@@ -1,3 +1,5 @@
-INSERT INTO "user" (id, username, password, role, first_name, last_name, deleted, created_at, updated_at)
-VALUES ('8fd71e4b-a428-4496-877f-ecbdad163713', 'user1', 'pass1', 'USER', 'Ivan', 'Ivanov', FALSE,
-        '2024-02-16T20:00:00.000+0300', '2024-02-16T20:00:00.000+0300');
+INSERT INTO user_entity (id, username, password, role, first_name, last_name, account_non_expired, account_non_locked,
+                         credentials_non_expired, enabled, created_at, updated_at)
+VALUES ('e8e20bfc-0d9c-49e7-b30f-1a6abf3b2795', 'username',
+        '$2a$10$JqrceZDuA3g/h7dLuHbrD.GRoJKZdjmVcNvJunSbOsk1yxGOPtIie', 'USER', 'Ivan', 'Ivanov', TRUE, TRUE,
+        TRUE, TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
